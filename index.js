705150d8c83282e920c9cb7c3aad039271921ed2
Alejandra Castillo <ahle.castillo@gmail.com>
2018-07-24 13:39:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(someValue) {
+function distanceFromHqInBlocks(someValue) { return 'distance in blocks'
 }
 
 function distanceFromHqInFeet(someValue){
