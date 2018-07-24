c3df8d9f44aa377161f9590c9f6e07033ec7d279
Alejandra Castillo <ahle.castillo@gmail.com>
2018-07-24 14:48:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,14 @@ function distanceTravelledInFeet(x,y){
 }
 function calculatesFarePrice() {
   
+  if (someValue <= 400){
+    return "This one is on me!";
+ } 
+  if (someValue >= 2000 && someValue <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+    else if (someValue > 2500); {
+    return "No can do.";
+  }
+}
 }
