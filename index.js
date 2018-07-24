61e74d2d2e8cb0ad0c26bb5a09ef4217718a6d56
Alejandra Castillo <ahle.castillo@gmail.com>
2018-07-24 14:52:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function calculatesFarePrice(x,y) {
     return 0;
  } 
   if (someValue >= 2000 && someValue <= 2500) {
-    return (((Math.abs(x-y))* 264) * 2)
+    return (((Math.abs(x-y))* 264) * 2);
   }
     else if (someValue > 2500); {
     return "No can do.";
