9022d6789a1a08be2a73e3d0685d77cf8a8334bd
Alejandra Castillo <ahle.castillo@gmail.com>
2018-07-24 14:05:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(someValue) { return Math.abs(someValue - 42);
+function distanceFromHqInBlocks(someValue) {return Math.abs(someValue - 42);
 }
 
 function distanceFromHqInFeet(someValue){
